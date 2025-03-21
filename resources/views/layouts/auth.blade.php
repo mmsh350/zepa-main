7c4fa2327df742e65701ffc44e8341ace6b283b6
Muhammad S. Hamidu <mhamidu@swglobal.local>
2025-03-21 19:11:14 +0100

    update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,42 +1,44 @@
-
 <!DOCTYPE html>
-<html lang="en" dir="ltr" data-nav-layout="vertical" data-vertical-style="overlay" data-theme-mode="light" data-header-styles="light" data-menu-styles="light" data-toggled="close">
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-vertical-style="overlay" data-theme-mode="light"
+    data-header-styles="light" data-menu-styles="light" data-toggled="close">
 
 <head>
     <!-- Meta Data -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Easy Verifications for your Business"/>
+    <meta name="description" content="Easy Verifications for your Business" />
     <meta name="keywords" content="NIMC, BVN, ZEPA, Verification, Airtime,Bills, Identity">
     <meta name="author" content="Zepa Developers">
     <title>ZEPA Solutions - @yield('title')</title>
 
-     <!-- fav icon -->
+    <!-- fav icon -->
     <link rel="icon" href="{{ asset('assets/home/images/favicon/favicon.ico') }}" type="image/x-icon">
 
     <!-- Main Theme Js -->
-    <script src="{{ asset('assets/js/authentication-main.js')}}"></script>
+    <script src="{{ asset('assets/js/authentication-main.js') }}"></script>
 
     <!-- Bootstrap Css -->
-    <link id="style" href="{{ asset('assets/libs/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" >
+    <link id="style" href="{{ asset('assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Style Css -->
-    <link href="{{ asset('assets/css/styles.min.css') }}" rel="stylesheet" >
+    <link href="{{ asset('assets/css/styles.min.css') }}" rel="stylesheet">
 
     <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" >
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet">
 
     <!-- Custom Css -->
-    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet" >
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
 </head>
+@stack('page-css')
+
 <body>
     <!-- start preLoader -->
-  <div id="preloader">
-	 <span class="loader"></span>
-  </div>
-  <!-- end preLoader -->
-@yield('content')
+    <div id="preloader">
+        <span class="loader"></span>
+    </div>
+    <!-- end preLoader -->
+    @yield('content')
 
     <!-- Jquery-->
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
@@ -47,10 +49,11 @@
     <!-- Show Password JS -->
     <script src="{{ asset('assets/js/show-password.js') }}"></script>
 
-   <!-- Page Specific JS -->
-    @yield('page-js')
+    <!-- Page Specific JS -->
+    @stack('page-js')
 
-     <!-- Config JS -->
+    <!-- Config JS -->
     <script src="{{ asset('assets/js/config.js') }}"></script>
 </body>
+
 </html>
